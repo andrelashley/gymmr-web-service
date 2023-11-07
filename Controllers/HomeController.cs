f4c16f4aed07faa5cbf94e31524a6303fdd8774a
André Lashley <andre.lashley@gmail.com>
2023-11-06 19:26:44 -0800

    Finish initial login flow

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -29,14 +29,42 @@ namespace Gymmr.Controllers
         }
 
         [HttpPost]
-        public IActionResult Login(LoginViewModel model) 
+        public async Task<IActionResult> Login(LoginViewModel model)
         {
-            return View();    
+            //if (ModelState.IsValid)
+            //{
+            //    var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
+
+            //    if (result.Succeeded)
+            //    {
+            //        if (Request.Query.Keys.Contains("ReturnUrl"))
+            //        {
+            //            Redirect(Request.Query["ReturnUrl"].First()!);
+            //        }
+            //        else
+            //        {
+            //            return RedirectToAction("Controller", "App");
+            //        }
+            //    }
+            //}
+
+            //ModelState.AddModelError("", "Failed to login");
+
+            //return View();
+
+            return RedirectToAction("Index", "App");
         }
 
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");   
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
